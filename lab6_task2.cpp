3ef22c8e58f35011bcd3f397e3e439d452c764ce
rouhan14 <rfaisal.bscs21seecs@seecs.edu.pk>
2022-10-18 18:36:53 +0500

    Second Commit with second tasks first part

--- lab6_task2.cpp
+++ lab6_task2.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 bool moveMin(vector<int> &in, vector<int> &out){
     bool status = true;
+
     for (int i = 0; i < in.size(); i++){
         out.push_back(in[i]);
     }
+    
     for (int i = 0; i < out.size(); i++){
         for (int j = 0; j < out.size() - 1; j++){
             if (out[j] > out[i]){
@@ -18,17 +20,10 @@ bool moveMin(vector<int> &in, vector<int> &out){
     }
 
     for (int i = 0; i < in.size(); i++){
-        if ( in[i] != out[i]){
+        if ( in[i] != out[i] ){
             status = false;
         }
     }
-
-    return status;
-}
-
-bool testMoveMin(vector<int> &in, vector<int> &out){
-    bool status;
-    
 
     return status;
 }
@@ -37,6 +32,7 @@ int main(){
     srand(time(0));
     vector<int> vec1;
     vector<int> vec2;
+    
     for (int i = 0; i < 7; i++){
         vec1.push_back(rand() % 100);
     }
@@ -50,9 +46,10 @@ int main(){
             }
         }
     }
-    vec1.push_back(rand() % 70);
+    
+    vec1.push_back(rand() % 101);
 
-    cout << moveMin(vec1, vec2) << endl;
+    cout << "Output from my sorting function:\t" << moveMin(vec1, vec2) << endl;
 
     return 0;
 }
