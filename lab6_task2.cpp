280f647366ca450b6509f1e77deeb5bf4dfab9a2
rouhan14 <rfaisal.bscs21seecs@seecs.edu.pk>
2022-10-19 16:32:27 +0500

    Second Task: Now with comments.

--- lab6_task2.cpp
+++ lab6_task2.cpp
@@ -5,11 +5,13 @@ bool moveMin(vector<int> &in, vector<int> &out)
 {
     bool status = true;
 
+    // Pushing the first vector elements into the second vector.
     for (int i = 0; i < in.size(); i++)
     {
         out.push_back(in[i]);
     }
 
+    // Sorting the second vector using bubble sort as mentioned in the lab task.
     for (int i = 0; i < out.size(); i++)
     {
         for (int j = 0; j < out.size() - 1; j++)
@@ -40,17 +42,26 @@ bool moveMin(vector<int> &in, vector<int> &out)
 bool testMoveMin(){
     bool status = true;
 
-    srand(time(0));
+    /* Creating three vector:
+        vec1 -> all elements are sorted except the last element.
+        vec2 -> all elements are sorted using bubble sort. (Done in the moneMin function).
+        vec3 -> all elements are sorted using stl::sort() function.
+    */
+
+    srand(time(0)); // to randomize the value everytime we take it.
     vector<int> vec1;
     vector<int> vec2;
     vector<int> vec3;
 
+    // Pushing random elements in the first vector.
     for (int i = 0; i < 7; i++){
         vec1.push_back(rand() % 100);
     }
 
+    // Sorting the first vector.
     sort(vec1.begin(), vec1.end());
 
+    // Pushing a random element in the vectoor as mentioned in the lab.
     vec1.push_back(rand() % 101);
 
     // Sorting using sort function created from bubble sort.
