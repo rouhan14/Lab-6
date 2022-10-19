b2e858a1541340b35c2cd2aff666deef32817609
rouhan14 <rfaisal.bscs21seecs@seecs.edu.pk>
2022-10-19 16:45:08 +0500

    Final code of lab3 with comments

--- lab_6_task3.cpp
+++ lab_6_task3.cpp
@@ -5,15 +5,20 @@ bool moveMin(vector<int> &in, vector<int> &out)
 {
     bool status = true;
 
+    // Pushing the first vector elements into the second vector.
     for (int i = 0; i < in.size(); i++)
     {
         out.push_back(in[i]);
     }
 
+
+    // Sorting the vector as stated in the lab task.
     int temp;
     int pos = out.size() - 1;
-    for (int i = out.size() - 2; i > -1; i--){
-        if (out[pos] < out[ i ]){
+    for (int i = out.size() - 2; i > -1; i--)
+    {
+        if (out[pos] < out[i])
+        {
             temp = out[i];
             out[i] = out[pos];
             out[pos] = temp;
@@ -21,7 +26,9 @@ bool moveMin(vector<int> &in, vector<int> &out)
         }
     }
 
-    for (int i = 0; i < in.size(); i++){
+    // Comparing vectors that was sorted by bubble sort and the vector itself.
+    for (int i = 0; i < in.size(); i++)
+    {
         if (in[i] != out[i])
         {
             status = false;
@@ -32,23 +39,61 @@ bool moveMin(vector<int> &in, vector<int> &out)
     return status;
 }
 
-bool testMoveMin(vector<int> &in, vector<int> &out){
+bool testMoveMin()
+{
     bool status = true;
 
-    for (int i = 0; i < in.size(); i++)
+    /* Creating three vector:
+        vec1 -> all elements are sorted except the last element.
+        vec2 -> all elements are sorted using bubble sort. (Done in the moneMin function).
+        vec3 -> all elements are sorted using stl::sort() function.
+    */
+
+    srand(time(0)); // to randomize the value everytime we take it.
+    vector<int> vec1;
+    vector<int> vec2;
+    vector<int> vec3;
+
+    // Pushing random elements in the first vector.
+    for (int i = 0; i < 7; i++)
     {
-        out.push_back(in[i]);
+        vec1.push_back(rand() % 100);
     }
 
-    sort(out.begin(), out.end());
+    // Sorting the first vector.
+    sort(vec1.begin(), vec1.end());
 
-    for (int i = 0; i < out.size(); i++)
+    // Pushing a random element in the vectoor as mentioned in the lab.
+    vec1.push_back(rand() % 101);
+
+    // Sorting using sort function created from bubble sort.
+    bool bubble = moveMin(vec1, vec2);
+
+    // Sorting using sort function.
+    for (int i = 0; i < vec1.size(); i++)
     {
-        if (in[i] != out[i])
+        vec3.push_back(vec1[i]);
+    }
+    sort(vec3.begin(), vec3.end());
+
+    // Comparing vectors that was sorted by sort function and the vector itself.
+    for (int i = 0; i < vec1.size(); i++)
+    {
+        if (vec1[i] != vec3[i])
         {
             status = false;
             break;
         }
+    }
+
+    // Comparing if the bubble sort and the sort function gave the same ans.
+    if (bubble == status)
+    {
+        status = true;
+    }
+    else
+    {
+        status = false;
     }
 
     return status;
@@ -56,28 +101,8 @@ bool testMoveMin(vector<int> &in, vector<int> &out){
 
 int main()
 {
-    
-    for (int i = 0; i < 7; i++)
-    {
-        vec1.push_back(rand() % 100);
-    }
-    for (int i = 0; i < 7; i++)
-    {
-        for (int j = 0; j < 6; j++)
-        {
-            if (vec1[j] > vec1[i])
-            {
-                int temp;
-                temp = vec1[j];
-                vec1[j] = vec1[i];
-                vec1[i] = temp;
-            }
-        }
-    }
 
-    vec1.push_back(rand() % 100);
-
-    cout << "Output from my sorting function:\t" << moveMin(vec1, vec2) << endl;
+    cout << "Test case answer: " << testMoveMin() << endl;
 
     return 0;
 }
